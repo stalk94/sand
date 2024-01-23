57ffea060a30884674c62e232756cb3fe465fac4
pigson <stalk9424@gmail.com>
2024-01-23 22:39:40 +0300

    modified:   src/engineHooks.tsx

--- src/engineHooks.tsx
+++ src/engineHooks.tsx
@@ -33,6 +33,17 @@ export function useInfoToolbar(type: "error"|"sucess"|"warn", title: string, tex
  * @param callback 
  */
 export function fetchApi(url: string, data: any, callback: Function) {
-    if(url && globalState.user) send(url, {login: globalState.user.login.get(), token: globalState.user.token.get(), ...data}).then(callback);
+    if(url && globalState.user) {
+        let call = callback;
+        if(url==="addTodo" || url==="readTodo" || url==="delTodo"){
+            call =(data)=> {
+                globalState.user.todo.set(data);
+                if(callback) callback(data);
+            }  
+        }
+
+        send(url, {login: globalState.user.login.get(), token: globalState.user.token.get(), ...data})
+            .then(call);
+    }
 }
 
