679fab9f24cb47ee5497bec381458b1a9c586f9c
pigson <stalk9424@gmail.com>
2024-02-03 23:14:57 +0300

    modified:   src/component/calendar.jsx 	modified:   src/style/calendar.css

--- src/component/calendar.jsx
+++ src/component/calendar.jsx
@@ -2,45 +2,117 @@ import React from "react";
 import globalState from "../global.state";
 import { useHookstate } from "@hookstate/core";
 import { useInfoToolbar, fetchApi, useToolbar, getDays } from "../engineHooks";
-import { Calendar } from 'primereact/calendar';
-import { DataTable } from 'primereact/datatable';
-import { Column } from 'primereact/column';
+import { Dropdown } from 'primereact/dropdown';
+import { Button } from 'primereact/button';
 import { useDidMount } from 'rooks';
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 import "../style/calendar.css"
 
 
-const Cell =({index, day})=> {
+const DateCalendarPicker =({curentDate, useDate})=> {
+    const month = ["январь","февраль","март","апрель","май","июнь","июль","август","сентябрь","октябрь","ноябрь","декабрь"];
+    const [arrYear, setArrYear] = React.useState([]);
+
+    useDidMount(()=> {
+        const arr = [curentDate[0]];
+        for(let i=0; i<6; i++) {
+            if(i!==0) arr.push({label:curentDate[0] + i, value:curentDate[0] + i});
+        }
+        for(let i=0; i<4; i++) {
+            if(i!==0) arr.unshift({label:curentDate[0] - i, value:curentDate[0] - i});
+        }
+
+        setArrYear(arr);
+    });
+    const useClick =(val)=> {
+        if(val==="left"){
+            if(curentDate[1]===0) useDate([curentDate[0]-1, 11]);
+            else useDate([curentDate[0], curentDate[1]-1]);
+        }
+        else {
+            if(curentDate[1]===11) useDate([curentDate[0]+1, 0]);
+            else useDate([curentDate[0], curentDate[1]+1]);
+        }
+    }
+
+    return (
+        <div className="navBar">
+            <Button onClick={()=> useClick("left")} className="p-button-rounded p-button-secondary p-button-outlined" label={<FaAngleLeft/>} />
+            <Dropdown style={{marginLeft:"15px"}} value={curentDate[0]} options={arrYear} onChange={(ev)=> useDate([ev.value, curentDate[1]])} />
+            <div style={{marginTop:"5px",marginLeft:"15px",marginRight:"15px"}}>
+                { month[curentDate[1]] }
+            </div>
+            <Button onClick={()=> useClick("right")} className="p-button-rounded p-button-secondary p-button-outlined" label={<FaAngleRight/>} />
+        </div>
+    );
+}
+const GridWeek =()=> {
+    const days = ["пн","вт","ср","чт","пт","сб","вс"];
+
+    return (
+        <div className="weeks">
+            {days.map((val, index)=> <div key={index} className="week">{val}</div>)}   
+        </div>
+    );
+}
+const Cell =({events, day, date})=> {
+    const useClickEvent =(ev)=> {
+
+    }
+    const useClickCell =()=> {
+
+    }
+
 
     return(
-        <div className="row"
-        >
+        <div className="row" onClick={useClickCell}>
             { day.day }
+            <div className="eventColumn">
+                {events.map((ev, index)=> {
+                    return <div className="eventCell"
+                        key={index}
+                        style={{backgroundColor:ev.color}}
+                        onClick={()=> useClickEvent(ev)}
+                    >
+
+                    </div>
+                })}
+            </div>
         </div>
     );
 }
 const GridCalendar =({date})=> {
-    const [data, setData] = React.useState(getDays(date[0], date[1]));
+    const [gridData, setGridData] = React.useState(getDays(date[0], date[1]));
+    const [data, setData] = React.useState([]);
 
+   
     React.useEffect(()=> {
-        setData(getDays(date[0], date[1]));
+        setGridData(getDays(date[0], date[1]));
+        fetchApi("getCalendar", {year:date[0], month:date[1]}, (res)=> {
+            if(res.error) useInfoToolbar("error", "Error", res.error);
+            else setData(res);
+        });
     }, date);
-    const render =()=> {
-        const rend = data.map((chank, index)=> {
-            return <div key={index} className="base">
-                {chank.map((day, indexDay)=> {
-                    if(day===null) return <div key={indexDay} className="nullRow"></div>
-                    else return <Cell key={indexDay} day={day}/>
-                })}
-            </div>
+    const useEvent =(day)=> {
+        const result = [];
+        data.forEach((event, index)=> {
+            if(day.day===event.day) result.push(event);
         });
 
-        return rend
+        return result;
     }
 
     
     return(
-        <div style={{display:"grid", width:"100%", height: "100%"}}>
-            {render()}
+        <div style={{display:"grid", width:"100%", height:"100%"}}>
+            {gridData.map((chank, index)=> {
+                return <div key={index} className="base">
+                    {chank.map((day, indexDay)=> {
+                        if(day===null) return <div key={indexDay} className="nullRow"></div>
+                        else return <Cell events={useEvent(day)} key={indexDay} date={date} day={day}/>
+                    })}
+                </div>
+            })}
         </div>
     );
 }
@@ -51,13 +123,15 @@ export default function BaseContainer() {
     const [date, setDate] = React.useState([new Date().getFullYear(), new Date().getMonth()])
 
     useDidMount(()=> {
-        useToolbar();
+        useToolbar(".");
     });
 
 
     return(
-        <>
+        <div style={{display:"flex",flexDirection:"column",width:"100%"}}>
+            <DateCalendarPicker curentDate={date} useDate={setDate}/>
+            <GridWeek/>
             <GridCalendar date={date}/>
-        </>
+        </div>
     );
 }
