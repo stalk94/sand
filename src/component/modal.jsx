4cf267674920f7b99bf7dc6cfffba1bacf5b5082
pigson <stalk9424@gmail.com>
2024-02-05 17:09:32 +0300

    modified:   src/component/modal.jsx 	new file:   src/style/modal.css

--- src/component/modal.jsx
+++ src/component/modal.jsx
@@ -1,25 +1,35 @@
 import React from "react";
 import globalState from "../global.state";
 import { useHookstate } from "@hookstate/core";
+import { useDidMount, useWillUnmount } from 'rooks';
 import { useInfoToolbar, fetchApi, useToolbar } from "../engineHooks";
+import "../style/modal.css";
 
 
-export function ModalEventCalendar({}) {
+
+export function ModalEventCalendar() {
     const [value, setValue] = React.useState(false);
-    const [view, setView] = React.useState();
+    const [date, setDate] = React.useState([]);
+    const [events, setEvents] = React.useState([]);
 
-    const useCreateEvent =()=> {
-        
-    }
-    useDidMount(()=> {
-        EVENT.on("clickCell", (obj)=> {
-            setValue(true);
+    const useCreateEvent =(newEvent)=> {
+        fetchApi("addEvent", {date:{year:date[0],month:date[1]}, event:newEvent}, (res)=> {
+            if(res.error) useInfoToolbar("error", "Error", res.error);
+            else EVENT.emit("eventUpdate", res);
         });
-    });
+    }
+    const eventOn =(obj)=> {
+        setEvents(obj.events);
+        setDate(obj.date);
+        setValue(true);
+    }
+    useDidMount(()=> EVENT.on("clickCell", eventOn));
+    useWillUnmount(()=> EVENT.off("clickCell", eventOn));
+
 
     return(
-        <div style={{display:value?"":"none"}}>
-            { view }
+        <div className="modalContainer" style={{display:value?"":"none"}}>
+            
         </div>
     );
 }
